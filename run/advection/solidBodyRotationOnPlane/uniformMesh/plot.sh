0433d35e72f150063131d7e7d30203e17b96a74b
Hiroe Yamazaki <h.yamazaki@imperial.ac.uk>
2017-07-31 00:30:06 +0100

    add coarseUniformMesh

--- run/advection/solidBodyRotationOnPlane/uniformMesh/plot.sh
+++ run/advection/solidBodyRotationOnPlane/uniformMesh/plot.sh
@@ -2,7 +2,7 @@
 
 # Plot results
 time=0
-for field in T UT mesh monitor; do
+for field in T UT mesh monitor uniT; do
     gmtFoam $field -region pMesh -time $time':'
 done
 
@@ -11,7 +11,7 @@ globalSum T -region pMesh
 
 # Make links for animategraphics
 mkdir -p animategraphics
-for field in T UT mesh monitor; do
+for field in T UT mesh monitor uniT; do
     for time in [0-9]*; do
         t=`echo $time | awk {'print $1/50'}`
         ln -s ../$time/$field.pdf animategraphics/${field}_$t.pdf
