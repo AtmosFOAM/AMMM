0d5dca3b38826fa9b5e619cae4097d329e546b21
Hiroe Yamazaki <ax915962@sws02hs.act.rdg.ac.uk>
2017-10-06 16:51:57 +0100

    ready to change the equation

--- run/advection/solidBodyRotationOnPlane/advectionOT/plot.sh
+++ run/advection/solidBodyRotationOnPlane/advectionOT/plot.sh
@@ -1,15 +1,6 @@
 #!/bin/bash -e
 
 # Post process
-case=.
-postProcess -func vorticity -case $case
-writeuvw vorticity -case $case
-for time in [0-9]*; do
-    mv $case/$time/vorticityz $case/$time/vorticity
-    rm $case/$time/vorticity?
-done
-gmtFoam vorticity -case $case
-
 for time in [0-9]*; do
     gmtFoam -time $time mesh
     gmtFoam -time $time T
@@ -17,7 +8,7 @@ done
 
 # Make links for animategraphics
 mkdir -p animategraphics
-for field in vorticity mesh T; do
+for field in mesh T; do
     for time in [0-9]*; do
 	t=`echo $time | awk {'print $1/250'}`
 	ln -s ../$time/$field.pdf animategraphics/${field}_$t.pdf
