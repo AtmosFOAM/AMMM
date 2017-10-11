20c89da2ff81164b45e81d23b5c0a225f7862ab9
Hilary Weller <h.weller@reading.ac.uk>
2017-10-11 17:31:08 +0100

    Getting advectionOTFoam working

--- run/advection/solidBodyRotationOnPlane/advectionOT/run.sh
+++ run/advection/solidBodyRotationOnPlane/advectionOT/run.sh
@@ -12,6 +12,7 @@ cp -r constant/polyMesh constant/cMesh
 
 ## Create initial conditions
 cp -r init_0 0
+cp 0/T constant/T_init
 # set tracer
 setVelocityField -dict advectionDict
 setAnalyticTracerField -velocityDict advectionDict \
@@ -20,5 +21,9 @@ setAnalyticTracerField -velocityDict advectionDict \
 # Raise the mountain
 terrainFollowingMesh
 
+gmtFoam -time 0 UT
+gv 0/UT.pdf &
+
 ## Solve the SWE
 advectionOTFoam -fixedMesh >& log & sleep 0.01; tail -f log
+
