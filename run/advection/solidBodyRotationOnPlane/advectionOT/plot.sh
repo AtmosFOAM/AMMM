3f8402c2e9e1637cda4a11010f795b8fcafe9017
Hiroe Yamazaki <ax915962@sws02hs.act.rdg.ac.uk>
2017-10-14 10:12:18 +0100

    started monitorFunctionFromGradU5050

--- run/advection/solidBodyRotationOnPlane/advectionOT/plot.sh
+++ run/advection/solidBodyRotationOnPlane/advectionOT/plot.sh
@@ -5,6 +5,7 @@ for time in [0-9]*; do
     gmtFoam -time $time mesh
     gmtFoam -time $time T
     gmtFoam -time $time uniT
+    gmtFoam -time $time monitor
 done
 
 # Conservation of T
@@ -14,12 +15,11 @@ globalSum uniT
 # Make links for animategraphics
 mkdir -p animategraphics
 time=0
-for field in mesh T uniT; do
-	    ln -s ../$time/$field.pdf animategraphics/${field}_$time.pdf
-    done
+for field in mesh T uniT monitor; do
+	ln -s ../$time/$field.pdf animategraphics/${field}_$time.pdf
 done
 
-for field in mesh T uniT; do
+for field in mesh T uniT monitor; do
     for time in [1-9]*; do
 	    t=`echo $time | awk {'print $1/50'}`
 	    ln -s ../$time/$field.pdf animategraphics/${field}_$t.pdf
---
+++ src/monitorFunction/monitorFunctionFrom/monitorFunctionFromGradU5050.C
@@ -0,0 +1,103 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2014-2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "monitorFunctionFromGradU5050.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvCFD.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+defineTypeNameAndDebug(monitorFunctionFromGradU5050, 0);
+addToRunTimeSelectionTable(monitorFunctionFrom, monitorFunctionFromGradU5050, dictionary);
+
+
+// * * * * * * * * * Protected Member Functions * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField> Foam::monitorFunctionFromGradU5050::monitorBase
+(
+    const surfaceVectorField& Uf
+) const
+{
+    const fvMesh& mesh = Uf.mesh();
+    tmp<volScalarField> tmonitor
+    (
+        new volScalarField
+        (
+            IOobject(name(), Uf.instance(), mesh),
+            magSqr(fvc::grad(Uf))
+        )
+    );
+
+    return tmonitor;
+}
+
+
+Foam::tmp<Foam::volScalarField> Foam::monitorFunctionFromGradU5050::baseToMonitor
+(
+    const volScalarField& b
+) const
+{
+    const fvMesh& mesh = b.mesh();
+    tmp<volScalarField> tmonitor
+    (
+        new volScalarField
+        (
+            IOobject(name(), b.instance(), mesh),
+            sqrt(b/sqr(monBaseMax() - monBaseMin()) + 1)
+        )
+    );
+    tmonitor.ref() = min(tmonitor.ref(), monitorMax());
+
+    return tmonitor;
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::monitorFunctionFromGradU5050::monitorFunctionFromGradU5050
+(
+    const IOdictionary& dict
+)
+:
+    monitorFunctionFrom(dict),
+    monBaseMin_(dict.lookup("monitorBaseMin")),
+    monBaseMax_(dict.lookup("monitorBaseMax")),
+    monitorMax_(readScalar(dict.lookup("monitorMax")))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::monitorFunctionFromGradU5050::~monitorFunctionFromGradU5050()
+{}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
