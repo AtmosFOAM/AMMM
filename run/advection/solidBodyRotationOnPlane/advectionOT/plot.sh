7ff997b24f94df79f6063850dbdcd7b687d1bb82
Hiroe Yamazaki <ax915962@sws02hs.act.rdg.ac.uk>
2017-10-31 09:31:17 +0000

    tidy up

--- run/advection/solidBodyRotationOnPlane/advectionOT/plot.sh
+++ run/advection/solidBodyRotationOnPlane/advectionOT/plot.sh
@@ -3,24 +3,28 @@
 # Post process
 for time in [0-9]*; do
     gmtFoam -time $time mesh
+    gmtFoam -time $time A
     gmtFoam -time $time T
+    gmtFoam -time $time AT
     gmtFoam -time $time uniT
     gmtFoam -time $time monitor
 done
 
 # Conservation of T
+globalSum A
 globalSum T
-globalSum uniT
 globalSum AT
+globalSum uniT
+
 
 # Make links for animategraphics
 mkdir -p animategraphics
 time=0
-for field in mesh T uniT monitor; do
+for field in mesh A T AT uniT monitor; do
 	ln -s ../$time/$field.pdf animategraphics/${field}_$time.pdf
 done
 
-for field in mesh T uniT monitor; do
+for field in mesh A T AT uniT monitor; do
     for time in [1-9]*; do
 	    t=`echo $time | awk {'print $1/50'}`
 	    ln -s ../$time/$field.pdf animategraphics/${field}_$t.pdf
